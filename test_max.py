b7fe91cfcd0b7e770174999863b5f1797f982639
Ivan Papusha <ivan.papusha@gmail.com>
2017-08-21 22:31:54 -0500

    does not pass tests

--- test_max.py
+++ test_max.py
@@ -2,7 +2,7 @@ import numpy as np
 import amnet
 
 
-def main():
+def test1():
     # test the affine transformations
     w1 = np.array([[1, 0]])
     b1 = np.array([0])
@@ -19,6 +19,25 @@ def main():
     phimax = amnet.Mu(a1, a2, a3)
     print phimax
     print phimax.eval(np.array([1,2]))
+
+def test2():
+    x1 = amnet.Variable(1, name='x1')
+    x2 = amnet.Variable(1, name='x2')
+
+    a1 = x1
+    a2 = x2
+
+    w3 = np.array([[-1, 1]])
+    b3 = np.array([0])
+    a3 = amnet.AffineTransformation(w3, amnet.Vcat(x1, x2), b3)
+
+    phimax = amnet.Mu(a1, a2, a3)
+    print phimax
+    print phimax.eval(np.array([1, 2]))
+
+def main():
+    test1()
+    test2()
 
 if __name__ == '__main__':
     main()
