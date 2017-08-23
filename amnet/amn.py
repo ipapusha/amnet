354ae7b4283161743fe0ac51502faadf5dbc9de4
Ivan Papusha <ivan.papusha@gmail.com>
2017-08-23 12:21:10 -0500

    rewrite

--- amnet/amn.py
+++ amnet/amn.py
@@ -1,3 +1,81 @@
-class Amn:
-    pass
+import numpy as np
+import copy
 
+class Amn(object):
+    def __init__(self, outdim=0, indim=0):
+        self.outdim = outdim
+        self.indim = indim
+
+    def __add__(self, other):
+        return NotImplemented
+
+    def eval(self, inp):
+        return NotImplemented
+
+
+# # even though Weight acts in many ways like an Amn, it is not
+# # (by itself) a function, so it is *not* a subclass of Amn
+# class Weight(object):
+#     def __init__(self, outdim=0, indim=0, w=None):
+#         self.outdim = outdim
+#         self.indim = indim
+#         self.w = copy.deepcopy(w)
+#         if hasattr(w, 'shape'):
+#             (self.outdim, self.indim) = w.shape
+#
+#     def __str__(self):
+#         if self.w is None:
+#             return 'w(%d,%d)' % (self.outdim, self.indim)
+#         else:
+#             return str(self.w)
+#
+#     def eval(self, inp):
+#         return np.dot(self.w, inp)
+#
+#
+# # aka constant vector
+# class Bias(Amn):
+#     def __init__(self, b):
+#         self.b = np.copy(b).flatten()
+#         super(Bias, self).__init__(outdim=len(self.b), indim=0)
+#
+#     def __str__(self):
+#         return str(self.b)
+#
+#     def eval(self, inp=None):
+#         return self.b
+
+
+class Variable(Amn):
+    def __init__(self, outdim=1, name='Variable'):
+        # a variable has the same input and output sizes
+        super(Variable, self).__init__(outdim=outdim, indim=outdim)
+        self.name = name
+
+    def __str__(self):
+        return self.name + '(' + str(self.outdim) + ')'
+
+    def eval(self, inp):
+        assert self.indim == self.outdim
+        assert len(inp) == self.indim
+        return inp
+
+
+class AffineTransformation(Amn):
+    def __init__(self, w, x, b):
+        assert w.ndim == 2
+        super(AffineTransformation, self).__init__(outdim=w.shape[0], indim=w.shape[1])
+        assert len(b) == self.outdim
+        assert x.outdim == self.indim
+
+        self.w = np.copy(w)
+        self.x = x
+        self.b = np.copy(b).flatten()
+
+    def __str__(self):
+        return str(self.w) + ' * ' + str(self.x) + ' + ' + str(self.b)
+
+    def eval(self, inp):
+        xv = self.x.eval(inp)
+        assert len(xv) == self.indim
+        return np.dot(self.w, xv) + self.b
--- examples/example_max.py
+++ examples/example_max.py
@@ -1,9 +1,29 @@
+import numpy as np
 import amnet
 
 
 def main():
-    print 'example is running'
-    phi = amnet.Amn()
+    x = amnet.Variable(2, name='x')
+    a1 = amnet.AffineTransformation(
+        np.array([[1, 0]]),
+        x,
+        np.array([0])
+    )
+    a2 = amnet.AffineTransformation(
+        np.array([[0, 1]]),
+        x,
+        np.array([0])
+    )
+    a3 = amnet.AffineTransformation(
+        np.array([[-1, 1]]),
+        x,
+        np.array([0])
+    )
+
+    print x
+    print a1
+    print a2
+    print a3
 
 if __name__ == '__main__':
     main()
