dbd5cc3871e36dff0c758e72e6b108392d8e5564
Ivan Papusha <ivan.papusha@gmail.com>
2017-08-30 16:45:17 -0500

    using matlab preconditioning

---
+++ examples/example_vgc_prescale.m
@@ -0,0 +1,33 @@
+% calculations for converting L = PC to discrete time
+
+% plant transfer function
+p = 1.0;
+P_tf = tf([0.05, 1.0], [1.0, -p]) * tf([1], [0.01, 1.0])^2;
+
+% linear controller transfer function
+k_p = 4.0;
+f_lp = 10.0;
+beta_lp = 0.7;
+f_i = 0.5;
+C_tf = k_p * tf([1.0, 2.0*pi*f_i], [1.0, 0.0]) * ...
+    tf([1.0], [1.0/(2*pi*f_lp)^2, (2.0*beta_lp)/(2*pi*f_lp), 1.0]);
+
+% unscaled system
+L = P_tf * C_tf;
+sys = ss(L);
+%[sys_scaled, info] = prescale(sys);
+
+sysd = ss(c2d(L, 0.001, 'zoh'));
+A = sysd.A;
+B = sysd.B;
+C = sysd.C;
+D = sysd.D;
+assert( all(sysd.D == 0) );
+
+dlmwrite('vgc_A.csv', A, 'precision', 12);
+dlmwrite('vgc_B.csv', B, 'precision', 12);
+dlmwrite('vgc_C.csv', C, 'precision', 12);
+dlmwrite('vgc_D.csv', D, 'precision', 12);
+
+% check step response
+%step(feedback(L,1));
