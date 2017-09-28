db6ecdd432a4951048ddf349254f2c993f6e252a
Ivan Papusha <ivan.papusha@gmail.com>
2017-09-28 14:54:53 -0500

    premerge

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 PYTHONPATH=. python tests/test_atoms.py
-#PYTHONPATH=. python tests/test_smt.py
-#PYTHONPATH=. python tests/test_lyap.py
+PYTHONPATH=. python tests/test_smt.py
+PYTHONPATH=. python tests/test_lyap.py
