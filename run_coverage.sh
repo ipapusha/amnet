07c28e918ba83719f2d6009123aa15d53f4e73e5
Ivan Papusha <ivan.papusha@gmail.com>
2017-10-04 14:41:24 -0500

    TST: added coverage script

---
+++ run_coverage.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# pip install coverage
+set -e
+coverage erase
+PYTHONPATH=. coverage run -a --source=. tests/test_atoms.py
+PYTHONPATH=. coverage run -a --source=. tests/test_smt.py
+PYTHONPATH=. coverage run -a --source=. tests/test_tf.py
+coverage report -m
+coverage html
+
+# consider also: (from https://stackoverflow.com/q/34728734)
+#python -m unittest discover -s tests/
+#coverage run -m unittest discover -s tests/
+#coverage report -m
+
