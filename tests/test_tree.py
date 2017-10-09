3afd83c2f101ec5975aa5d4b2554c6fbc866c636
Ivan Papusha <ivan.papusha@gmail.com>
2017-10-09 15:05:56 -0500

    TST: descendants()

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,6 +1,7 @@
 import numpy as np
 import amnet
 import amnet.vis
+import amnet.tree
 
 from numpy.linalg import norm
 
@@ -31,9 +32,70 @@ class TestTree(unittest.TestCase):
         #z3.set_param('smt.case_split', 5)
         #z3.set_param('smt.relevancy', 2)
 
-    def test_tree_descendants(self):
-        pass
+    def test_tree_descendants_dag(self):
+        xyz = amnet.Variable(3, name='xyz')
+        x = amnet.atoms.select(xyz, 0)
+        yz = amnet.Linear(
+            np.array([[0, 1, 0], [0, 0, 1]]),
+            xyz
+        )
 
+        maxyz = amnet.atoms.max_all(yz)
+        twoxp1 = amnet.Affine(
+            np.array([[2]]),
+            x,
+            np.array([1])
+        )
+        twox = amnet.atoms.add2(x, x)
+        threex = amnet.atoms.add2(x, twox)
+        fivexp1 = amnet.atoms.add2(twoxp1, threex)
+        phi = amnet.atoms.add2(fivexp1, maxyz)
+
+        # visualize dag
+        if VISUALIZE: amnet.vis.quick_vis(phi, title='tree_dag')
+
+        # simple membership tests
+        # NOTE: May need to convert to any(x is d for d in desc)
+        desc = amnet.tree.descendants(phi)
+        # number of descendents may change
+        # depending on implementation of max
+        # self.assertEqual(len(desc), 17)
+        self.assertGreaterEqual(len(desc), 9)
+        self.assertTrue(xyz in desc)
+        self.assertTrue(x in desc)
+        self.assertTrue(yz in desc)
+        self.assertTrue(maxyz in desc)
+        self.assertTrue(twoxp1 in desc)
+        self.assertTrue(twox in desc)
+        self.assertTrue(threex in desc)
+        self.assertTrue(fivexp1 in desc)
+        self.assertTrue(phi in desc)
+
+        # descendants list must contain unique pointers
+        self.assertEqual(
+            len(desc),
+            len(set(id(d) for d in desc))
+        )
+
+        # descendants list has exactly one variable
+        self.assertEqual(
+            len([d for d in desc if isinstance(d, amnet.Variable)]),
+            1
+        )
+
+        # this dag has only one mu
+        self.assertEqual(
+            len([d for d in desc if isinstance(d, amnet.Mu)]),
+            1
+        )
+
+        # true dag represents 5*x + 1 + max(y, z)
+        # so it should have at least 3 + 1 = 4 (x, y, z + outside add)
+        # affine transformations
+        self.assertGreaterEqual(
+            len([d for d in desc if isinstance(d, amnet.Affine)]),
+            4
+        )
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestTree)
