e96ca5e6bf1aef381986f56ed21661d5f29594cf
Ivan Papusha <ivan.papusha@gmail.com>
2017-10-09 14:22:25 -0500

    working on direct tree manipulation

--- amnet/tree.py
+++ amnet/tree.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy.linalg import norm
 import amnet
 
+from collections import deque
+
 import copy
 
 """
@@ -10,7 +12,8 @@ Contains routines for manipulating and simplifying Amn trees
 
 FPTOL=1e-8
 
-def compose_rewire(phi1, x1, phi2, x2):
+
+def compose_rewire(phi1, phi2):
     """
     Given two AMNs and pointers to their input variables,
     creates a new AMN that is a composition of the two.
@@ -23,9 +26,88 @@ def compose_rewire(phi1, x1, phi2, x2):
 
     Note: the variable of the new AMN is x2
     """
-    # TODO: implement
-    pass
+    assert phi1.indim == phi2.outdim
 
+    # base case: variable node
+    # throw away node
+    if isinstance(phi1, amnet.Variable):
+        return
+
+    # base case: already did the rewire
+    if phi1 == phi2:
+        return
+
+    # depth-first: somewhere in the middle
+    # 1. recurse down
+    if hasattr(phi1, 'x'):
+        compose_rewire(phi1.x, phi2)
+    if hasattr(phi1, 'y'):
+        compose_rewire(phi1.y, phi2)
+    if hasattr(phi1, 'z'):
+        compose_rewire(phi1.z, phi2)
+
+    # 2. change the indim
+    phi1.indim = phi2.outdim
+
+    # 3. if child child is a variable, rewire it
+    # rewire phi1's variable to point to phi2
+    if hasattr(phi1, 'x') and isinstance(phi1.x, amnet.Variable):
+        phi1.x = phi2
+    if hasattr(phi1, 'y') and isinstance(phi1.y, amnet.Variable):
+        assert isinstance(phi1, amnet.Mu) or \
+               isinstance(phi1, amnet.Stack)
+        phi1.y = phi2
+    if hasattr(phi1, 'z') and isinstance(phi1.z, amnet.Variable):
+        assert isinstance(phi1, amnet.Mu)
+        phi1.z = phi2
+
+
+def children(phi):
+    """
+    returns a (possibly empty) list of all direct children
+    of the node phi
+    """
+    ret = []
+    if hasattr(phi, 'x'):
+        ret.append(phi.x)
+    if hasattr(phi, 'y'):
+        assert isinstance(phi, amnet.Mu) or \
+               isinstance(phi, amnet.Stack)
+        ret.append(phi.y)
+    if hasattr(phi, 'z'):
+        assert isinstance(phi, amnet.Mu)
+        ret.append(phi.z)
+
+    assert len(ret) <= 3
+    return ret
+
+
+def descendants(phi):
+    """
+    returns a list of all descendants of phi,
+    including phi itself
+    """
+    q = deque([phi])  # queue of nodes to check
+    d = list()        # list of descendants
+
+    while q:
+        node = q.popleft()
+        # cannot use not(node in d) because Python's `in`
+        # checks for `==` or `is` equality;
+        # we want only `is` equality
+        if not(any(node is e for e in d)):
+            # node is new
+            d.append(node)
+            # add its children to check for reachability
+            q.extend(children(node))
+
+    # done finding reachable nodes
+    return d
+
+
+################################################################################
+# ABANDONED METHODS
+################################################################################
 
 def simplify(phi):
     """
@@ -39,8 +121,9 @@ def simplify(phi):
     """
 
     # 1. only manipulate the copy
-    phic = copy.deepcopy(phi)
-    return phic
+    #phic = copy.deepcopy(phi)
+    #return phic
+    pass
 
 def eval_ones(phi):
     """
