0404543303fff697cf8e026d1273fb013909056a
Ivan Papusha <ivan.papusha@gmail.com>
2017-12-06 11:03:25 -0600

    lyap tests

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,5 @@ PYTHONPATH=. python tests/test_atoms.py
 PYTHONPATH=. python tests/test_smt.py
 PYTHONPATH=. python tests/test_tree.py
 PYTHONPATH=. python tests/test_smt_relu.py
-#PYTHONPATH=. python tests/test_lyap.py
+PYTHONPATH=. python tests/test_lyap.py
 #PYTHONPATH=. python tests/test_tf.py
